27e9a36a6bbe73de57378a06ec8ca0d41d606c6b
Yuriy C <chkanov2015@gmail.com>
2021-04-24 13:47:18 +0500

    realization rest_api

--- water/settings.py
+++ water/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 import dj_database_url
 db_from_env = dj_database_url.config()
