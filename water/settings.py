0c178454418769bdb433a8fe585524c4d0904f39
Yuriy C <chkanov2015@gmail.com>
2021-04-24 13:58:33 +0500

    realization rest_api

--- water/settings.py
+++ water/settings.py
@@ -127,7 +127,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    # Add to this list all the locations containing your static files
+)
+
 
 import dj_database_url
 db_from_env = dj_database_url.config()
