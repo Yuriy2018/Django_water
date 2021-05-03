4aa32674b109d17beec44d37dfad5a837e264c81
Yuriy C <chkanov2015@gmail.com>
2021-05-03 13:05:48 +0500

    fix

--- common/models.py
+++ common/models.py
@@ -104,7 +104,7 @@ class Client(models.Model):
     number_apart = models.CharField(max_length=10, verbose_name='Номер кв.', blank=True, null=True)
     address = models.CharField(max_length=100, verbose_name='Адрес', blank=True, null=True)
     object_name = models.CharField(max_length=100, verbose_name='Название объекта', blank=True, null=True)
-    phone_number = models.CharField(max_length=15, verbose_name='Телефон', null=True)
+    phone_number = models.CharField(max_length=30, verbose_name='Телефон', null=True)
     driver = models.ForeignKey(Driver, verbose_name='Водитель', on_delete=models.PROTECT, null=True)
     # first_name = models.CharField(max_length=50, verbose_name='Имя', blank=True, null=True)
     # last_name = models.CharField(max_length=50, verbose_name='Фамилия', blank=True, null=True)
