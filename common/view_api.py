e903265dd2ad0fbb0470cce1b6f628658bf9bc2c
Yuriy C <chkanov2015@gmail.com>
2021-05-20 18:30:27 +0500

    Rest get orders for driver

--- common/view_api.py
+++ common/view_api.py
@@ -82,7 +82,7 @@ class OrdersForDriver(APIView):
         if not driver:
             return Response({"info":"Driver not fount"},status=200)
 
-        orders = Order.objects.filter(Q(client__driver=driver[0]) | ~Q(status_order= Order.STATUS_TYPE_COMPLETED)).order_by('-date')
+        orders = Order.objects.filter(Q(client__driver=driver[0]) & ~Q(status_order= Order.STATUS_TYPE_COMPLETED)).order_by('-date')
         if not orders:
             return Response({"info":"Orders not fount"},status=200)
 
