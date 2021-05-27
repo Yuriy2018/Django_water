bde0b388e8d965696a706eb81f8dd3a263cf16a7
Yuriy C <chkanov2015@gmail.com>
2021-05-27 17:06:00 +0500

    add filter 'not_load_1C' in REST

--- documents/admin.py
+++ documents/admin.py
@@ -20,7 +20,7 @@ class OrderAdmin(admin.ModelAdmin):
     date_wiev.short_description = 'Дата документа'
 
     # list_display = ('id', 'time_seconds',)
-    list_display = ('number', 'date_wiev', 'client', 'show_driver', 'amount', 'number1С', 'new_client', 'create_bot', 'user',)
+    list_display = ('number', 'date_wiev', 'client', 'show_driver', 'amount', 'number1С', 'new_client', 'create_bot', 'user', 'not_load_1C')
     list_display_links = ('number', 'date_wiev', 'client',)
     inlines = [TabluarOrdersInline, ]
     fields = [('number', 'number1С'),'date', 'status_order','client', 'type_play', 'amount', 'comment', 'returned_container',('user', 'not_load_1C')]
