7a6f443f0d76e0198de3bc78b7b3952962e414ae
Yuriy C <chkanov2015@gmail.com>
2021-07-14 21:26:12 +0500

    adding report packet

--- common/view_api.py
+++ common/view_api.py
@@ -54,7 +54,9 @@ class ClientFullData(APIView):
         driver = client.driver
         if driver:
             open_orders = list(driver.get_open_orders_full())
-            open_ord = [str(x['date_dev']) for x in open_orders]
+            # open_ord = [str(x['date_dev']) for x in open_orders]
+            open_ord = [str(x.date_dev) for x in open_orders]
+
             c = Counter(open_ord)
             dict_open_ord = dict(c)
 
